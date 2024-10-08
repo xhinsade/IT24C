5dcce16749aa2b5854f3be2d242e6646d0b72675
xhinsade <nekochii57@gmail.com>
2024-10-08 18:45:12 +0800

    Added the initTileLayer

--- App Gallery.html/applet2.js
+++ App Gallery.html/applet2.js
@@ -3,4 +3,12 @@ class LeafletMap {
     this.map = L.map(containerId).setView(center, zoom);
     this.initTileLayer();
      }
+     
+     initTileLayer() {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(this.map);
+  }
+  
   }
