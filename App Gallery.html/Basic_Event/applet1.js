00007c2d96f8d2004329dec6ae32b09b16db2ad0
xhinsade <nekochii57@gmail.com>
2024-10-17 22:19:30 +0800

    Added function of the input text

--- App Gallery.html/Basic_Event/applet1.js
+++ App Gallery.html/Basic_Event/applet1.js
@@ -1,3 +1,15 @@
+//Event Function 1
+function updateDisplay() {
+    var textbox = document.getElementById('textbox');
+    var displayText = document.getElementById('displayText');
+    displayText.textContent = textbox.value;
+}
+
+function showAlert() {
+    var textbox = document.getElementById('textbox');
+    alert("Hello " + textbox.value);
+}
+
 // Function to save user input data
 function saveData() {
     const name = document.getElementById("name").value;
