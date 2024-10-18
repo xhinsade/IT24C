2f130aee320f38508c6f9ff62b71e0dccd2bacc8
xhinsade <nekochii57@gmail.com>
2024-10-18 21:33:20 +0800

    updated the functionality of the input text

--- App Gallery.html/Basic_Event/applet1.js
+++ App Gallery.html/Basic_Event/applet1.js
@@ -75,3 +75,15 @@ function clearDisplayedData() {
     // Optionally, you can call displayData here if you want to show the empty state immediately
     displayData();
 }
+
+//Event Function 1
+function updateDisplay() {
+    var textbox = document.getElementById('textbox');
+    var displayText = document.getElementById('displayText');
+    displayText.textContent = textbox.value;
+}
+
+function showAlert() {
+    var textbox = document.getElementById('textbox');
+    alert("Hello " + textbox.value);
+}
