fb9305732f80f75eb5add911874a3395f81bc7ca
xhinsade <nekochii57@gmail.com>
2024-11-01 18:57:00 +0800

    added input text

--- App Gallery.html/OpenWeather_API/applet5.js
+++ App Gallery.html/OpenWeather_API/applet5.js
@@ -1,5 +1,12 @@
 class Weather {
   constructor() {
+    
+    //API Key
     this.apiKey = document.getElementById('apiKeyInput');
+    
+    //Text Input
+    this.cityInput = document.getElementById('cityInput');
+    this.getWeatherBtn = document.getElementById('getWeatherBtn');
+
   }
 }
