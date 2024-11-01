80eb5a476bb76c45efd1b59b9b16d2f8ac7cd27d
xhinsade <nekochii57@gmail.com>
2024-11-01 19:09:34 +0800

    adding getters for display weather

--- App Gallery.html/OpenWeather_API/applet5.js
+++ App Gallery.html/OpenWeather_API/applet5.js
@@ -26,7 +26,12 @@ class WeatherApp {
   }
   
   displayWeather(data){
-    
+    this.cityName.textContent = `${data.name}, ${data.sys.country} (${data.coord.lat}, ${data.coord.lon})`;
+        this.temperature.textContent = `Temperature: ${data.main.temp} °C`;
+        this.description.textContent = `Weather: ${data.weather[0].description}`;
+        this.humidity.textContent = `Humidity: ${data.main.humidity}%`;
+        this.windSpeed.textContent = `Wind Speed: ${data.wind.speed} m/s`;
+        
 }
 
   
