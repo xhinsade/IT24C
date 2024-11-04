319d0fd418eed978976d2a47725cf89484602f38
xhinsade <nekochii57@gmail.com>
2024-11-04 13:05:00 +0800

    adding action lister click to the button

--- App Gallery.html/Todo_List/applet6.js
+++ App Gallery.html/Todo_List/applet6.js
@@ -6,3 +6,10 @@ const addButton = document.getElementById("addButton");
 
 let tasks = []; 
 let editIndex = -1; 
+
+addButton.addEventListener("click", () => {
+    const taskText = document.getElementById("todoInput").value.trim();
+    const taskTime = document.getElementById("todoTime").value;
+    const taskLocation = document.getElementById("todoLocation").value.trim();
+
+});
