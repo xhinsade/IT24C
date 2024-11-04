0c342dad665ca4be3005c1a96268e083896823f1
xhinsade <nekochii57@gmail.com>
2024-11-04 13:03:16 +0800

    instantiating arrays

--- App Gallery.html/Todo_List/applet6.js
+++ App Gallery.html/Todo_List/applet6.js
@@ -2,3 +2,7 @@ const todoList = document.getElementById("todoList");
 const accomplishedList = document.getElementById("accomplishedList");
 const canceledList = document.getElementById("canceledList");
 const addButton = document.getElementById("addButton");
+
+
+let tasks = []; 
+let editIndex = -1; 
