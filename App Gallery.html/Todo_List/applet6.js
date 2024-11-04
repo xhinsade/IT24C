732698199c6bcc029fefb400e575465ee9f54026
xhinsade <nekochii57@gmail.com>
2024-11-04 13:02:41 +0800

    getting instances from the web

---
+++ App Gallery.html/Todo_List/applet6.js
@@ -0,0 +1,4 @@
+const todoList = document.getElementById("todoList");
+const accomplishedList = document.getElementById("accomplishedList");
+const canceledList = document.getElementById("canceledList");
+const addButton = document.getElementById("addButton");
